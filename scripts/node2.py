6fce21eef1b7e97ba52b1d8f553f3f8c7f32c768
Muhammad Tamer <muhammadbassiony@gmail.com>
2020-09-06 23:32:24 +0200

    multithreaded

--- scripts/node2.py
+++ scripts/node2.py
@@ -21,8 +21,42 @@ def callback(data):
     #print(msg)
     rospy.loginfo(""+msg)
 
+def callback2(data):
+    #rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.data)
+    #print(data.data)
+
+    t1 = threading.Thread(target=thread_fsm2, args=(data,))
+    t2 = threading.Thread(target=thread_fsm2, args=(data,))
+    t3 = threading.Thread(target=thread_fsm2, args=(data,))
+    t4 = threading.Thread(target=thread_fsm2, args=(data,))
+    t5 = threading.Thread(target=thread_fsm2, args=(data,))
+
+    msg = " "
+    if(data.data == "1"):
+        t1.start()
+        t1.join()
+    elif(data.data == "2"):
+        t2.start()
+        t2.join()
+    elif(data.data == "3"):
+        t3.start()
+        t3.join()
+    elif(data.data == "4"):
+        t4.start()
+        t4.join()
+    elif(data.data == "5"):
+        t5.start()
+        t5.join()
+
+    #print(msg)
+    #rospy.loginfo(""+msg)
+
 def thread_fsm(data):
     print("entered state %s\t%s" % (str(data.data), threading.current_thread()))
+    rospy.loginfo("entered state %s\t%s" % (str(data.data), threading.current_thread()))
+
+def thread_fsm2(data):
+    print("entered state %s" % (str(data.data)))
     rospy.loginfo("entered state %s\t%s" % (str(data.data), threading.current_thread()))
     
 def listener():
@@ -34,7 +68,8 @@ def listener():
     # run simultaneously.
     rospy.init_node('node2', anonymous=True)
 
-    rospy.Subscriber("chatter", String, thread_fsm)
+#    rospy.Subscriber("chatter", String, thread_fsm)
+    rospy.Subscriber("chatter", String, callback2)
 
     # spin() simply keeps python from exiting until this node is stopped
     rospy.spin()
